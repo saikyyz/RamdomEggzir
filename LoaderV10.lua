1427abbdfa9813f698ad5190e97fc1ed8e88d8f3
saikyyz <jokerft549@gmail.com>
2025-07-21 23:11:18 +0800

    Update LoaderV10.lua

--- LoaderV10.lua
+++ LoaderV10.lua
@@ -1,1 +1,2 @@
-
+loadstring(game:HttpGet("https://raw.githubusercontent.com/saikyyz/saikigrow/refs/heads/main/dadadada.lua"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/NStxL8R0ji/0", true))()
